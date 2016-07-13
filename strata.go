d7a47f2ee68d3d5c00ed8f5a7b5f8f7ce8625802
William Goodall <wgoodall01@gmail.com>
2016-07-13 15:42:14 -0400

    Add csv output

--- strata.go
+++ strata.go
@@ -3,9 +3,11 @@ package main
 import (
 	"math/rand"
 	"time"
-	"flag"
+	"bufio"
+	"os"
 	"fmt"
 	"math"
+	"github.com/alecthomas/kingpin"
 )
 
 
@@ -14,9 +16,17 @@ var Rng = rand.New(rand.NewSource(time.Now().UnixNano()));
 
 func main(){
 	// argv parsing to config
-	iterations := flag.Int("iterations", 1000000, "Number of iterations, default 1 million.")
+	arg_iterations := kingpin.Flag("iterations", "Number of iterations").
+		Default("10").
+		Short('i').
+		Int()
 
-	realAvg := Sample(ALL)
+	arg_output := kingpin.Flag("output", "Output file location").
+		Default(""). // If output is blank, send to stdout.
+		Short('o').
+		String()
+	kingpin.Parse()
+
 
 	//Sampling method average declarations
 	errorAverages := map[Strategy]*Average{
@@ -26,7 +36,27 @@ func main(){
 		STRATIFIED_COL: &Average{},
 	}
 
-	for i := 0; i < *iterations; i++ {
+	msgRaw("Taking %d samples... ", *arg_iterations)
+	start := time.Now()
+
+	//Open output file or stdout and create buffer
+	file := os.Stdout
+	if *arg_output != "" {
+		var err error
+		file, err = os.Create(*arg_output)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+	}
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	//Get the real average, and save it to the output
+	realAvg := Sample(ALL)
+	writeRecord(writer, ALL, realAvg.Value())
+
+	for i := 0; i < *arg_iterations; i++ {
 		for strat := SIMPLE_RANDOM; strat <= STRATIFIED_COL; strat++ {
 			//Get sample avg
 			sampleAvg := Sample(strat)
@@ -37,15 +67,34 @@ func main(){
 			//Add the absolute value of the error to the error average
 			absError := math.Abs(errorMargin)
 			errorAverages[strat].Include(absError)
+
+			//Add the absolute value of the error to the output buffer.
+			writeRecord(writer, strat, sampleAvg.Value())
 		}
 	}
 
-	fmt.Printf("Real          : %f\n", realAvg.Value())
-	fmt.Printf("Simple random : %f%% error\n", errorAverages[SIMPLE_RANDOM].Value() * 100)
-	fmt.Printf("Convenience   : %f%% error\n", errorAverages[CONVENIENCE].Value() * 100)
-	fmt.Printf("Stratified row: %f%% error\n", errorAverages[STRATIFIED_ROW].Value() * 100)
-	fmt.Printf("Stratified col: %f%% error\n", errorAverages[STRATIFIED_COL].Value() * 100)
+	msg("[DONE in %dms]", time.Since(start).Nanoseconds() / 1000000);
+	msg("")
 
+	msg("  --- Error Digest ---")
+	msg("Real          : %f avg", realAvg.Value())
+	msg("Simple random : %f%% error", errorAverages[SIMPLE_RANDOM].Value() * 100)
+	msg("Convenience   : %f%% error", errorAverages[CONVENIENCE].Value() * 100)
+	msg("Stratified row: %f%% error", errorAverages[STRATIFIED_ROW].Value() * 100)
+	msg("Stratified col: %f%% error", errorAverages[STRATIFIED_COL].Value() * 100)
+}
+
+func msg(msg string, a ...interface{}){
+	msgRaw(msg + "\n", a...)
+}
+
+func msgRaw(msg string, a ...interface{}){
+	//Writes a formatted message to stderr
+	fmt.Fprintf(os.Stderr, msg, a...)
+}
+
+func writeRecord(writer *bufio.Writer, strat Strategy, average float64) (int, error){
+	return writer.WriteString(fmt.Sprintf("%d,%f\n", int(strat), average))
 }
 
 
